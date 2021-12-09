c2b3da725dfd239aa1a367280a9b5faabed5b5c9
Linus Berglund <linus@berglund.me>
2021-12-09 23:37:11 +0100

    solve day09 part 2

--- src/day09/solution.py
+++ src/day09/solution.py
@@ -1,6 +1,3 @@
-from pprint import pprint
-
-
 def part1(data):
     heatmap = [[int(location) for location in line] for line in data]
     risk_levels = [[False for location in line] for line in data]
@@ -31,8 +28,82 @@ def part1(data):
     return sum(risk)
 
 
+def find_low_points(heightmap):
+    risk = []
+
+    for line_index, line in enumerate(heightmap):
+        for location_index in range(len(heightmap[line_index])):
+
+            current = heightmap[line_index][location_index]
+            left, right, up, down = 9, 9, 9, 9
+
+            if location_index - 1 >= 0:
+                left = heightmap[line_index][location_index - 1]
+            if location_index < len(line) - 1:
+                right = heightmap[line_index][location_index + 1]
+            if line_index - 1 >= 0:
+                up = heightmap[line_index - 1][location_index]
+            if line_index < len(data) - 1:
+                down = heightmap[line_index + 1][location_index]
+
+            if current < left and current < right and current < up and current < down:
+                risk.append((line_index, location_index))
+
+    return risk
+
+
+def traverse_basin(heightmap, searched: set, point: tuple, basins):
+
+    if point in searched:
+        return
+
+    searched.add(point)
+
+    (x, y) = point
+
+    left, up, down, right = (9, 9, 9, 9)
+
+    if y - 1 >= 0:
+        left = heightmap[x][y - 1]
+    if x - 1 >= 0:
+        up = heightmap[x - 1][y]
+    if x < len(heightmap) - 1:
+        down = heightmap[x + 1][y]
+    if y < len(heightmap[0]) - 1:
+        right = heightmap[x][y + 1]
+
+    if sum([left, up, down, right]) >= 9 * 4:
+        return
+
+    if left < 9:
+        traverse_basin(heightmap, searched, (x, y - 1), basins)
+    if up < 9:
+        traverse_basin(heightmap, searched, (x - 1, y), basins)
+    if down < 9:
+        traverse_basin(heightmap, searched, (x + 1, y), basins)
+    if right < 9:
+        traverse_basin(heightmap, searched, (x, y + 1), basins)
+
+    basins[x][y] = True
+    return basins
+
+
 def part2(data):
-    pass
+    heightmap = [[int(location) for location in line] for line in data]
+    low_points = find_low_points(heightmap)
+
+    basin_sizes = []
+
+    for point in low_points:
+        searched = set()
+        basins = [[False for location in line] for line in data]
+        basins = traverse_basin(heightmap, searched, point, basins)
+        basin_sizes.append((sum(map(sum, basins))))
+
+    basin_sizes.sort(reverse=True)
+    basin_sizes = basin_sizes[:3]
+
+    return basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
 
 
 data = open("src/day09/input.txt", "r").readlines()
