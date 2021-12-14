4836980e895919c0418381e8460f4baebecfa2be
Linus Berglund <linus@berglund.me>
2021-12-14 18:57:26 +0100

    solve day14 part 2

--- src/day14/solution.py
+++ src/day14/solution.py
@@ -18,7 +18,7 @@ def create_template_tuples(template):
     return template_tuples
 
 
-def step(template, rules: dict):
+def step(template: list, rules: dict):
     tt = create_template_tuples(template)
     new_tuples = []
 
@@ -34,7 +34,7 @@ def step(template, rules: dict):
     new_string = ""
     for t in new_tuples:
         if len(t) == 3:
-            a, b, c = t
+            a, b, _ = t
             new_string += a + b
         if len(t) == 2:
             a, b = t
@@ -59,19 +59,68 @@ def check_quantity(template):
 
 def part1(data):
     (template, rules) = read_input(data)
-    for i in range(10):
-        print(i)
+    for _ in range(10):
         template = step(template, rules)
 
     return check_quantity(template)
 
 
+################################################################################
+
+
+def step_part2(template_counter: dict, rules: dict):
+
+    next_template_counter = template_counter.copy()
+
+    for rule, value in rules.items():
+        if not template_counter.get(rule):
+            continue
+
+        occurences = template_counter[rule]
+
+        next_template_counter[(rule[0], value)] = (
+            next_template_counter.get((rule[0], value), 0) + occurences
+        )
+        next_template_counter[(value, rule[1])] = (
+            next_template_counter.get((value, rule[1]), 0) + occurences
+        )
+
+        next_template_counter[rule] -= occurences
+
+    return next_template_counter
+
+
+def check_quantity_part2(template, last_polymer):
+    counter = dict()
+
+    for (a, _), value in template.items():
+        counter[a] = counter.get(a, 0) + value
+
+    counter[last_polymer] = counter.get(last_polymer, 0) + 1
+
+    highest = max([x for x in counter.values()])
+    lowest = min([x for x in counter.values()])
+
+    return highest - lowest
+
+
 def part2(data):
-    pass
+    (template, rules) = read_input(data)
+
+    last_polymer = template[-1]
+    new_template = dict()
+
+    for current_tuple in create_template_tuples(template):
+        new_template[current_tuple] = new_template.get(current_tuple, 0) + 1
+
+    for _ in range(40):
+        new_template = step_part2(new_template, rules)
+
+    return check_quantity_part2(new_template, last_polymer)
 
 
 data = open("src/day14/input.txt", "r").readlines()
-data = open("src/day14/example.txt", "r").readlines()
+# data = open("src/day14/example.txt", "r").readlines()
 
 data = [line.strip("\n") for line in data]
 
